525bad99590fd77174bacbfdb4638cdaebd05cb6
phamlequocphi-1609 <phamlequocphi16092001@gmail.com>
2024-10-07 18:27:42 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -13,44 +13,66 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group([
     'namespace'=>'Api'
 ], function(){
-    // member00
-    Route::post('/user/login', [MemberApiController::class, 'login']);
-    Route::post('/user/register', [MemberApiController::class, 'register']);
+    // member
+    Route::prefix('/user')->group(function(){
+        Route::post('/login', [MemberApiController::class, 'login']);
+        Route::post('/register', [MemberApiController::class, 'register']);
+    });
+    
+    //country
     Route::get('/country', [MemberApiController::class, 'country']);
+    
     // Blog
-    Route::get('/blog/list', [BlogApiController::class, 'blogList']);
-    Route::get('/blog/new/post', [BlogApiController::class, 'blogNew']);
-    Route::get('/blog/detail/{id}', [BlogApiController::class, 'detail']);
+    Route::prefix('/blog')->group(function(){
+        Route::get('/list', [BlogApiController::class, 'blogList']);
+        Route::get('/new/post', [BlogApiController::class, 'blogNew']);
+        Route::get('/detail/{id}', [BlogApiController::class, 'detail']);
+        Route::get('/detail-pagination/{id}', [BlogApiController::class, 'paginationBlogDetail']);
+    });
+   
+    
     Route::get('/rate/blog/{id}', [BlogApiController::class, 'rateBlog']);
-    Route::get('/blog/detail-pagination/{id}', [BlogApiController::class, 'paginationBlogDetail']);
+
     // Category-brand
     Route::get('/category-brand', [ProductApiController::class, 'categoryBrand']);
     // Product
     Route::get('/rate/product/{id}', [ProductApiController::class, 'getProductRate']);
-    Route::get('/product', [ProductApiController::class, 'productHome']);
-    Route::get('/product/all', [ProductApiController::class,'productall']);
-    Route::get('/product/new', [ProductApiController::class, 'newProduct']);
-    Route::get('/product/sale', [ProductApiController::class, 'saleProduct']);
-    Route::get('/product/wishlist', [ProductApiController::class, 'wishlist']);
-    Route::get('/product/detail/{id}',[ProductApiController::class, 'productDetail']);
-    Route::post('/product/cart',[ProductApiController::class, 'cart']);
-    Route::get('/product/recommend',[ProductApiController::class,'productRecommend']);
-    Route::get('/product/search',[ProductApiController::class, 'productSearch']);
-    Route::get('/product/priceRange', [ProductApiController::class, 'productPriceRange']);
-    Route::get('/product/searhcAdvanced', [ProductApiController::class,'productSearchAdvanced']); 
+
+    Route::prefix('/product')->group(function(){
+        Route::get('/', [ProductApiController::class, 'productHome']);
+        Route::get('/all', [ProductApiController::class,'productall']);
+        Route::get('/new', [ProductApiController::class, 'newProduct']);
+        Route::get('/sale', [ProductApiController::class, 'saleProduct']);
+        Route::get('/wishlist', [ProductApiController::class, 'wishlist']);
+        Route::get('/detail/{id}',[ProductApiController::class, 'productDetail']);
+        Route::post('/cart',[ProductApiController::class, 'cart']);
+        Route::get('/recommend',[ProductApiController::class,'productRecommend']);
+        Route::get('/search',[ProductApiController::class, 'productSearch']);
+        Route::get('/priceRange', [ProductApiController::class, 'productPriceRange']);
+        Route::get('/searhcAdvanced', [ProductApiController::class,'productSearchAdvanced']); 
+    });
+  
     // send mail
     Route::post('/sendmail', [MailController::class,'send']);
     
     Route::middleware(['auth:sanctum'])->group(function(){
-        Route::post('/product/comment/{id}', [ProductApiController::class, 'comment']);
-        Route::post('/blog/comment/{id}', [BlogApiController::class, 'comment']);
-        Route::post('/blog/rate/{id}', [BlogApiController::class, 'createRate']);
-        Route::post('/user/update/{id}', [MemberApiController::class,'update']);
-        Route::get('/user/my-product', [ProductApiController::class, 'productShow']);
-        Route::post('/user/product/add', [ProductApiController::class, 'productAdd']);
-        Route::get('/user/product/{id}', [ProductApiController::class, 'show']);
-        Route::post('/user/product/update/{id}', [ProductApiController::class, 'edit']);
-        Route::get('/user/product/delete/{id}',[ProductApiController::class, 'remove']);
-        Route::post('/product/rate/{id}',[ProductApiController::class,'productRate']);
+        Route::prefix('/product')->group(function(){
+            Route::post('/comment/{id}', [ProductApiController::class, 'comment']);
+            Route::post('/rate/{id}',[ProductApiController::class,'productRate']);
+        });
+      
+        Route::prefix('/blog')->group(function(){
+            Route::post('/comment/{id}', [BlogApiController::class, 'comment']);
+            Route::post('/rate/{id}', [BlogApiController::class, 'createRate']);
+        });
+
+        Route::prefix('/user')->group(function(){
+            Route::post('/update/{id}', [MemberApiController::class,'update']);
+            Route::get('/my-product', [ProductApiController::class, 'productShow']);
+            Route::post('/product/add', [ProductApiController::class, 'productAdd']);
+            Route::get('/product/{id}', [ProductApiController::class, 'show']);
+            Route::post('/product/update/{id}', [ProductApiController::class, 'edit']);
+            Route::get('/product/delete/{id}',[ProductApiController::class, 'remove']);
+        });
     });
 });
